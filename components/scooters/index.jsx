923dceb338f45a127b8125fa667194863e19a9e3
Victor <gamesvictor1928@gmail.com>
2022-10-18 15:12:02 -0300

    Removida 3 primeiras imagens
    - 3 primeiras scooters removidas do site
    - Footer responsivizado

--- components/scooters/index.jsx
+++ components/scooters/index.jsx
@@ -1,8 +1,5 @@
 import Image from 'next/image'
 
-import PatineteP10 from "../../public/images/scooters/PatineteP10.png"
-import ScooterLN20 from "../../public/images/scooters/ScooterLN20.png"
-import ScooterM2 from "../../public/images/scooters/scooterM2.png"
 import ScooterMC20 from "../../public/images/scooters/scooterMC20.png"
 import Scooters9 from "../../public/images/scooters/scooters9.png"
 import Scooterx11 from "../../public/images/scooters/scooterx11.png"
@@ -10,38 +7,10 @@ import Scooterx11 from "../../public/images/scooters/scooterx11.png"
 import { ScooterImg, ScootersDiv, ScootersS } from "./style.module.scss"
 
 export default function Scooters(props) {
-    function mapear({img, title}, i) {
-        return  <div className={ScooterImg} key={i}>
-            <Image
-                src={img}
-                key={i}
-                layout="responsive"
-                
-            />
-            <p>{title}</p>
-        </div>
-    }
-
     return (
         <section className={ScootersS}>
             <h2>Scooters Elétricas Novas e usadas a partir de R$ 4.998,00</h2>
             <div className={ScootersDiv}>
-                {
-                    [
-                        {
-                            img: PatineteP10,
-                            title: "Patinete P10"
-                        },
-                        {
-                            img: ScooterLN20,
-                            title: "Scooter LN20"
-                        },
-                        {
-                            img: ScooterM2,
-                            title: "Scooter M2"
-                        }
-                    ].map(mapear)
-                }
                 {
                     [
                         {
@@ -56,7 +25,17 @@ export default function Scooters(props) {
                             img: Scooterx11,
                             title: "Scooter x11"
                         }
-                    ].map(mapear)
+                    ].map(({ img, title }, i) => {
+                        return <div className={ScooterImg} key={i}>
+                            <Image
+                                src={img}
+                                key={i}
+                                layout="responsive"
+
+                            />
+                            <p>{title}</p>
+                        </div>
+                    })
                 }
             </div>
         </section>
